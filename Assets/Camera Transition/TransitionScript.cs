54f26da338907c054013dc86fb0cb312b8a919ee
Kristián Kovalský <k.kovalsky@me.com>
2018-04-01 12:30:11 +0200

    Camera transition

---
+++ Assets/Camera Transition/TransitionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScript : MonoBehaviour {
+
+    public Transform[] views;
+    public float transSpeed;
+    Transform currentView;
+
+	void Start () {
+		
+	}
+
+	void Update() {
+		
+        if (Input.GetKeyDown (KeyCode.A)) {
+            currentView = views[0];
+        }
+        if (Input.GetKeyDown (KeyCode.B)) {
+            currentView = views[1];
+        }
+	}
+
+	void LateUpdate () {
+
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transSpeed);
+
+	}
+}
