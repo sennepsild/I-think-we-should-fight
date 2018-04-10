2bc31f86d653815f2fe4ffcbde023373abed47a0
Georgi <roofman990@gmail.com>
2018-04-10 14:00:21 +0200

    Georgi HUD
    Georgi HUD

---
+++ Assets/Georgi stuff/for crosshair - Georgi/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crosshairImage;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        Screen.lockCursor = true;
+
+	}
+
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
+}
