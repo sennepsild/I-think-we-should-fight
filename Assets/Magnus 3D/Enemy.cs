5fa52b3748d60579034120680a8205d48af654e1
Magnus <magnusmhp@gmail.com>
2018-04-17 07:34:07 +0200

    AI
    shinderu

--- Assets/Magnus 3D/Enemy.cs
+++ Assets/Magnus 3D/Enemy.cs
@@ -4,26 +4,143 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Enemy : MonoBehaviour {
 
+
+    float duration = 0;
+    public GameObject ExplosionPrefab;
+
+    public Transform rFoot,lFoot;
+
+    public Transform playerTrans;
     public float maxHealth = 100;
     float health;
-    public Animator anim;
+    Animator anim;
+    bool Ultimating = false;
     public Image bar;
+     Fighter1 playerFigh;
 
     public AudioSource audio1;
+    public AudioSource shinderu;
+    public AudioSource Anthem;
 
     // Use this for initialization
     void Start () {
+        playerFigh = playerTrans.gameObject.GetComponent<Fighter1>();
+
+        anim = GetComponent<Animator>();
         health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (health > 50)
+        {
+            if (GetDistanceTOPlayer() > 1.4f)
+            {
+                if (duration <= 0)
+                {
+                    transform.Translate(Vector3.forward * 0.5f * Time.deltaTime);
+                    anim.SetFloat("Horizontal", 0.5f);
+                }
+            }
+            else
+            {
+                anim.SetFloat("Horizontal", 0);
+                if (duration <= 0)
+                {
+                    duration += 1.5f;
+                    DoATTACK();
+                }
+
+            }
+        }
+        else
+        {
+            if (!Ultimating)
+            {
+                shinderu.Play();
+                Anthem.Play();
+            }
+            Ultimating = true;
+            anim.SetBool("Ultimate", true);
+        }
+
+        if (transform.position.z < playerTrans.position.z)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
         
-	}
+
+            if(duration>0)
+        duration -= Time.deltaTime;
+
+    }
+
+    public void DoATTACK()
+    {
+        anim.SetTrigger("EnemyKick");
+    }
+
+    public float GetDistanceTOPlayer()
+    {
+        float dist = transform.position.z - playerTrans.position.z;
+        if (dist < 0)
+            dist = dist * -1;
+        return dist;
+
+    }
+
+    public void KickHit()
+    {
+        GameObject exp = Instantiate(ExplosionPrefab);
+        exp.transform.position = rFoot.position;
+        Destroy(exp, 3);
+        playerFigh.PlayExplosionSOund();
+
+        if (CheckIfHit(rFoot))
+        {
+            playerFigh.DAMAGE();
+        }
+
+    }
+    public void UltiLeft()
+    {
+        GameObject exp = Instantiate(ExplosionPrefab);
+        exp.transform.position = lFoot.position;
+        Destroy(exp, 3);
+        playerFigh.PlayExplosionSOund();
+
+        if (CheckIfHit(lFoot))
+        {
+            playerFigh.DAMAGE();
+        }
+
+    }
+    public void UltiRight()
+    {
+        GameObject exp = Instantiate(ExplosionPrefab);
+        exp.transform.position = rFoot.position;
+        Destroy(exp, 3);
+        playerFigh.PlayExplosionSOund();
+
+        if (CheckIfHit(rFoot))
+        {
+            playerFigh.DAMAGE();
+        }
+
+    }
+
+
 
 
     public void Hit()
     {
+        print(anim.name);
+        if(!Ultimating)
         anim.SetTrigger("Hit");
         health-= 10;
         audio1.Play();
@@ -31,4 +148,20 @@ public class Enemy : MonoBehaviour {
         transform.Translate(Vector3.back*0.1f);
         bar.fillAmount = health / maxHealth;
     }
+
+
+    public bool CheckIfHit(Transform tPos)
+    {
+        float ZDistance = tPos.position.z - playerTrans.position.z;
+        if (ZDistance < 0)
+            ZDistance *= -1;
+        float YDistance = tPos.position.y - playerTrans.position.y;
+
+
+
+        if (ZDistance < 0.6 && YDistance < 1.85 && YDistance > 0)
+            return true;
+        return false;
+    }
+
 }
